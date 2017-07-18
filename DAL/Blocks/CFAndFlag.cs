b7f72328e47b2b970618be7d85635db181870184
Radoslaw Markiewicz <rmarkiewicz@ecovadis.com>
2017-07-18 17:23:27 +0200

    proper list handling

--- DAL/Blocks/CFAndFlag.cs
+++ DAL/Blocks/CFAndFlag.cs
@@ -1,11 +1,61 @@
-﻿namespace DAL.Blocks
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DAL.Blocks
 {
     public class CFAndFlag
     {
         public int ClientId { get; set; }
         public string ClientName { get; set; }
-        public string CF1s { get; set; }
-        public string CF2s { get; set; }
-        public string Flags { get; set; }
+        public string CF1s
+        {
+            get
+            {
+                return string.Join(";", _CF1List);
+            }
+            set
+            {
+                _CF1List = !string.IsNullOrWhiteSpace(value) ? value.Split(';').ToList() : new List<string>();
+            }
+        }
+        public string CF2s {
+            get {
+                return string.Join(";", _CF2List);
+            }
+            set {
+                _CF2List = !string.IsNullOrWhiteSpace(value) ? value.Split(';').ToList() : new List<string>();
+            }
+        }
+        public string Flags
+        {
+            get
+            {
+                return string.Join(";", _flagList);
+            }
+            set
+            {
+                _flagList = !string.IsNullOrWhiteSpace(value) ? value.Split(';').ToList() : new List<string>();
+            }
+        }
+
+        private List<string> _CF1List = new List<string>();
+        private List<string> _CF2List = new List<string>();
+        private List<string> _flagList = new List<string>();
+
+        public List<string> GetCF1List()
+        {
+            return _CF1List;
+        }
+
+        public List<string> GetCF2List()
+        {
+            return _CF2List;
+        }
+
+        public List<string> GetFlags()
+        {
+            return _flagList;
+        }
     }
 }
--- OneShotCFAndFlagsExporter/Program.cs
+++ OneShotCFAndFlagsExporter/Program.cs
@@ -13,10 +13,18 @@ namespace OneShotCFAndFlagsExporter
         {
             var rows = CFAndFlagRepository.GetRows();
 
+            var cf1Max = rows.Select(l => l.GetCF1List().Count).OrderByDescending(c => c).First();
+            var cf2Max = rows.Select(l => l.GetCF2List().Count).OrderByDescending(c => c).First();
+            var flagMax = rows.Select(l => l.GetFlags().Count).OrderByDescending(c => c).First();
+
+            var maxLength = Math.Max(flagMax, Math.Max(cf1Max, cf2Max));
+
             foreach (var row in rows)
             {
-                Console.WriteLine(row.ClientName);
+                Console.WriteLine(row.ClientName + " " + row.GetFlags().Count());
             }
+
+            Console.ReadKey();
         }
     }
 }
